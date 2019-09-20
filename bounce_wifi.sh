9544ef363e89be7245644655e9f90f7ff9d41edc
Niels Jespersen <niels@jespersen.info>
2019-09-20 07:51:08 +0200

    Refinements

--- bounce_wifi.sh
+++ bounce_wifi.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 # wifi bliver træt på Ubuntu 19.04 for hver nogle få 100 GB. Det hjælper at slukek/tænde radioen. 
 # Skal køre fx hver time via cron som root
+# Altså: 
+# sudo -s
+# crontab -e 
+# tast i bunden og gem: 0 * * * * /home/niels/projs/pypi-conda-cran-mirror/bounce_wifi.sh
+# kontroller crontab -l
+# exit root prompt
+# check at der kommer en fil /tmp/wifibounced.txt, som ændres hver time
 /usr/bin/echo off >> /tmp/wifibounced.txt
 /usr/bin/nmcli radio wifi off
 /usr/bin/nmcli radio wifi on
-/usr/bin/echo on >> /temp/wifibounced.txt
+/usr/bin/echo on >> /tmp/wifibounced.txt
--- bsn_ny.sh
+++ bsn_ny.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
+# pip install bandersnatch
+# kopier 
 cd ~/bsn
-/usr/bin/python3 ~/.local/bin/bandersnatch --config ~/bsn/bsn.conf  mirror  1> ~/bsn/log.txt 2>&1
+/usr/bin/python3 ~/anaconda3/bin/bandersnatch --config ~/bsn/bsn.conf  mirror  1> ~/bsn/log.txt 2>&1
 # Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
 find /datadrev/pypi -type f -name ":" -exec rm -f {};
 #
