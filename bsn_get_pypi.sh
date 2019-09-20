b29086c8816a9b9540eff0edb3a3bfaf065ad553
Niels Jespersen <niels@jespersen.info>
2019-09-20 07:53:43 +0200

    Refinements

--- bsn_get_pypi.sh
+++ bsn_get_pypi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # pip install bandersnatch
-# kopier 
+# kør dette script. Vent et par dage
 cd ~/bsn
 /usr/bin/python3 ~/anaconda3/bin/bandersnatch --config ~/bsn/bsn.conf  mirror  1> ~/bsn/log.txt 2>&1
 # Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
