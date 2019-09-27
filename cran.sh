8d8ceeb0ce733d8d7f5b79713ca35d3143d557e3
Niels Jespersen <niels@jespersen.info>
2019-09-27 14:34:03 +0200

    Quality-lifting

--- cran.sh
+++ cran.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-rsync -rtlzv --delete cran.r-project.org::CRAN /datadrev/cran
+rsync -rtlzv --delete cran.r-project.org::CRAN /datadrev/cran 1> ~/bsn/cran_log.txt 2>&1
 # rm -rf /datadrev/cran/bin/ (alt non-windows)
 #
---
+++ driver.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+~/bsn/bsn_get_pypi.sh 
+~/bsn/mirror.sh
+~/bsn/cran.sh
