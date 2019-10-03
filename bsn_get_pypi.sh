c3659d4438cc06d229842f38bd6b759014c44acd
Niels Jespersen <niels@jespersen.info>
2019-10-03 08:27:42 +0200

    refactored

--- bsn_get_pypi.sh
+++ bsn_get_pypi.sh
@@ -3,6 +3,4 @@
 # kør dette script. Vent et par dage
 cd ~/bsn
 ~/.local/bin/bandersnatch --config ~/bsn/bsn.conf  mirror  1> ~/bsn/log.txt 2>&1
-# Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
-find /datadrev/pypi -type f -name ":" -exec rm -f {}; 1>> ~/bsn/log.txt 2>>&1
 #
--- driver.sh
+++ driver.sh
@@ -3,3 +3,6 @@
 ~/bsn/mirror.sh
 ~/bsn/cran.sh
 #
+# Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
+find /datadrev -type f -name ":" -exec rm -f {}; 1>> ~/bsn/remove_colon_named_files.txt 2>>&1
+#
