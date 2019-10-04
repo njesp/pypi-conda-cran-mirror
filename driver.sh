298edb8e695cdbd176ec055cbc6d3828ba5ae9bd
Niels Jespersen <niels@jespersen.info>
2019-10-04 08:04:40 +0200

    Bug fixes

--- driver.sh
+++ driver.sh
@@ -4,5 +4,9 @@
 ~/bsn/cran.sh
 #
 # Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
-find /datadrev -type f -name ":" -exec rm -f {}; 1>> ~/bsn/remove_colon_named_files.txt 2>>&1
+find /datadrev -type f -name ":" -exec rm -vf {} \; 1> ~/bsn/remove_colon_named_files.txt 2>&1
+# Fjern 
+rm -rf /datadrev/cran/bin/linux
+rm -rf /datadrev/cran/bin/macos
+rm -rf /datadrev/cran/bin/macosx
 #
