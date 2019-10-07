47529a5e6b7f87d9d01ffbeada7d3d486478460d
Niels Jespersen <niels@jespersen.info>
2019-10-07 11:31:26 +0200

    changes

--- cran.sh
+++ cran.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
-rsync -rtlzv --delete cran.r-project.org::CRAN /datadrev/cran 1> ~/bsn/cran_log.txt 2>&1
-# rm -rf /datadrev/cran/bin/ (alt non-windows)
+rsync -rtlzv --delete cran.r-project.org::CRAN/bin/windows /datadrev/cran/bin/windows 1> ~/bsn/cran_log.txt 2>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/contrib /datadrev/cran/contrib 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/doc /datadrev/cran/doc 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/help /datadrev/cran/help 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/html /datadrev/cran/html 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/mirmon /datadrev/cran/mirmon 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/src /datadrev/cran/src 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/web /datadrev/cran/web 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
 #
--- driver.sh
+++ driver.sh
@@ -6,7 +6,7 @@
 # Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
 find /datadrev -type f -name ":" -exec rm -vf {} \; 1> ~/bsn/remove_colon_named_files.txt 2>&1
 # Fjern 
-rm -rf /datadrev/cran/bin/linux
-rm -rf /datadrev/cran/bin/macos
-rm -rf /datadrev/cran/bin/macosx
+#
+# Vis IO stats
+# dstat -tdD total,sdb 60
 #
