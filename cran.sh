86497d3bc25f07968f34140d4f4c17ddb3a22f23
Niels Jespersen <niels@jespersen.info>
2019-10-07 14:10:29 +0200

    Fixes

--- cran.sh
+++ cran.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
+mkdir /datadrev/cran/bin/windows
+mkdir /datadrev/cran/contrib
+mkdir /datadrev/cran/doc
+mkdir /datadrev/cran/help
+mkdir /datadrev/cran/html
+mkdir /datadrev/cran/mirmon
+mkdir /datadrev/cran/src
+mkdir /datadrev/cran/web
+
 rsync -rtlzv --delete cran.r-project.org::CRAN/bin/windows /datadrev/cran/bin/windows 1> ~/bsn/cran_log.txt 2>~/bsn/cran_log.txt
 rsync -rtlzv --delete cran.r-project.org::CRAN/contrib /datadrev/cran/contrib 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
 rsync -rtlzv --delete cran.r-project.org::CRAN/doc /datadrev/cran/doc 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
