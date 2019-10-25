fcee0f7958263c2ba9c35f02005f0ebd571a3ad2
Niels Jespersen <niels@jespersen.info>
2019-10-25 11:45:03 +0200

    small improvement

--- mirror.sh
+++ mirror.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd /datadrev
 # Mirror default channels
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o -> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/main/win-64" 
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/main/noarch" 
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/msys2/win-64"
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/msys2/noarch"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o -> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/main/win-64" 
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/main/noarch" 
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/msys2/win-64"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://repo.anaconda.com/pkgs/msys2/noarch"
 # Mirror conda-forge, skal ikke i default channels. 
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/win-64"
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/noarch"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/win-64"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/noarch"
 # Generelt kan det overvejes at tilføje følgende for at spare plads
 # -R --regex-type pcre --reject-regex '(.*py27.*)'
