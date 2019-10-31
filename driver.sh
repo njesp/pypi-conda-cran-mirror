c2e0baafd1cfa9581582e441d9a29bca3624990f
Niels Jespersen <niels@jespersen.info>
2019-10-31 07:11:12 +0100

    Improvements

--- driver.sh
+++ driver.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+touch ~/bsn/start_log.txt
 ~/bsn/bsn_get_pypi.sh 
 ~/bsn/mirror.sh
 ~/bsn/cran.sh
 #
 # Fjern filer med : i navnet. Dem bryder Windows sig virkelig ikke om. 
 find /datadrev -type f -name ":" -exec rm -vf {} \; 1> ~/bsn/remove_colon_named_files.txt 2> ~/bsn/remove_colon_named_files.txt
-# Fjern 
+touch ~/bsn/slut_log.txt
 #
 # Vis IO stats
 # dstat -tdD total,sdb 60
