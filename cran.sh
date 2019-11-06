55cbf5465c754dc1976c1b9d2affe87305321318
Niels Jespersen <niels@jespersen.info>
2019-11-06 07:13:38 +0100

    improvements

--- cran.sh
+++ cran.sh
@@ -8,12 +8,12 @@ mkdir -p /datadrev/cran/mirmon
 mkdir -p /datadrev/cran/src
 mkdir -p /datadrev/cran/web
 
-rsync -rtlzv --delete cran.r-project.org::CRAN/bin/windows /datadrev/cran/bin/windows 1> ~/bsn/cran_log.txt 2>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/contrib /datadrev/cran/contrib 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/doc /datadrev/cran/doc 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/help /datadrev/cran/help 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/html /datadrev/cran/html 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/mirmon /datadrev/cran/mirmon 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/src /datadrev/cran/src 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
-rsync -rtlzv --delete cran.r-project.org::CRAN/web /datadrev/cran/web 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/bin/windows /datadrev/cran/bin 1> ~/bsn/cran_log.txt 2>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/contrib /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/doc /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/help /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/html /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/mirmon /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/src /datadrev/cran/ 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
+rsync -rtlzv --delete cran.r-project.org::CRAN/web /datadrev/cran 1>> ~/bsn/cran_log.txt 2>>~/bsn/cran_log.txt
 #
