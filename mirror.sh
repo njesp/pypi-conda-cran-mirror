42cbd9632d7ab494588570c6938943e1c044ccb8
Niels Jespersen <niels@jespersen.info>
2019-11-06 12:25:23 +0100

    Added plotly channel

--- mirror.sh
+++ mirror.sh
@@ -8,5 +8,8 @@ wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -
 # Mirror conda-forge, skal ikke i default channels. 
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/win-64"
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/conda-forge/noarch"
+# Mirror plotly, skal ikke i default channels. 
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/plotly/win-64"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/plotly/noarch"
 # Generelt kan det overvejes at tilføje følgende for at spare plads
 # -R --regex-type pcre --reject-regex '(.*py27.*)'
