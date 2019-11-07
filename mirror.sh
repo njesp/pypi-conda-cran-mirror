6fbe1053776d74bb1129478ae955bb077e7e1dfb
Niels Jespersen <niels@jespersen.info>
2019-11-07 08:41:29 +0100

    added bioconda

--- mirror.sh
+++ mirror.sh
@@ -11,5 +11,12 @@ wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -
 # Mirror plotly, skal ikke i default channels. 
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/plotly/win-64"
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/plotly/noarch"
+# Mirror pytorch, , skal ikke i default channels.
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/pytorch/win-64"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/pytorch/noarch"
+# Mirror bioconda, skal ikke i default channels.
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/bioconda/win-64"
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -nv -o ->> ~/bsn/mirror_log.txt --user-agent="conda/4.5.11 requests/2.19.1 CPython/3.7.1 Windows/10 Windows/10.0.15063" "https://conda.anaconda.org/bioconda/noarch"
+
 # Generelt kan det overvejes at tilføje følgende for at spare plads
 # -R --regex-type pcre --reject-regex '(.*py27.*)'
