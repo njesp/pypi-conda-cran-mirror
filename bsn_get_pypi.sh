00083b6297a0ea89e9f9bb2ad286e343f76975b0
Niels Jespersen <niels@jespersen.info>
2020-10-03 15:55:31 +0200

    Added conda activate

--- bsn_get_pypi.sh
+++ bsn_get_pypi.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # pip3 install bandersnatch
 # kør dette script. Vent et par dage
+conda activate pypi-conda-cran-mirror
 cd ~/bsn
-~/.local/bin/bandersnatch --config ~/bsn/bsn.conf mirror 1> ~/bsn/log.txt 2>&1
+bandersnatch --config ~/bsn/bsn.conf mirror 1> ~/bsn/log.txt 2>&1
 #
